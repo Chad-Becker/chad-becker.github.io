16cfe67b798c37027fcf6523422f77a90142c3de
Chad Becker <cwb6vh@virginia.edu>
2016-04-22 13:23:22 -0500

    Added files via upload
    I fixed the smooth scroll feature to worth with the navbar links. I was disabling the default click behavior of the external links. I also fixed the pixel widths for background images to work on larger mobile devices (e.g. ipads). I also changed the position of the background images when they are viewed in a mobile browser since more of the images will be cut off. I also want to see if the slide-in animation works in Firefox. I've been developing and testing using Chrome, where the animation works, but it only works in Firefox when the website is viewed locally as opposed to being pulled from the GitHub Pages web server.

--- myScript.js
+++ myScript.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-// // ---------------------Smooth Scrolling----------------------------------
+// ---------------------Smooth Scrolling----------------------------------
 
-//   // Add smooth scrolling to all links in navbar + footer link
-//   $(".navbar a, a[href='#aboutMe']").on('click', function(event) {
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a.scroll, a[href='#aboutMe']").on('click', function(event) {
 
-//   // Prevent default anchor click behavior
-//   event.preventDefault();
+  // Prevent default anchor click behavior
+  event.preventDefault();
 
-//   // Store hash
-//   var hash = this.hash;
+  // Store hash
+  var hash = this.hash;
 
-//   // Using jQuery's animate() method to add smooth page scroll
-//   // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-//   $('html, body').animate({
-//   	scrollTop: $(hash).offset().top
-//   }, 2000, function(){
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+  	scrollTop: $(hash).offset().top
+  }, 2000, function(){
 
-//     // Add hash (#) to URL when done scrolling (default click behavior)
-//     window.location.hash = hash;
-// });
-// });
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+});
+});
 
 // ---------------------Popovers------------------------------------------
 
@@ -30,10 +30,10 @@ $('[data-toggle="popover"]').popover();
 
 $(window).scroll(function() {
   $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
+    var pos = $(this).offset().top - 700;
 
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 700) {
+    if (pos < winTop) {
       $(this).addClass("slide");
     }
   });
